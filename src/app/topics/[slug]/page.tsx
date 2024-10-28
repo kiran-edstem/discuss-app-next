70e12a3109f5d463d3b4f9725ac15de2073cdd1e
kiran-edstem <kiran.pv@edstem.com>
2024-10-28 09:50:46 +0530

    added create post functionality

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,7 +1,24 @@
-const TopicShowPage = () => {
+import PostCreateForm from "@/components/post/post-create-form";
+
+interface TopicShowPageProps {
+    params: Promise<{
+        slug: string;
+    }>;
+}
+
+
+const TopicShowPage = async ({ params }: TopicShowPageProps) => {
+    const { slug } = await params;
     return (
-        <div>
-            TopicShowPage
+        <div className="grid grid-cols-4 gp-4 p-4">
+            <div className="col-span-3">
+                <h1 className="text-2xl font-bold mb-2">
+                    {slug}
+                </h1>
+            </div>
+            <div>
+                <PostCreateForm slug={slug} />
+            </div>
         </div>
     );
 }
